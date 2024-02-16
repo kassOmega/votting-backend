4a833ca7f1a38c852b7bab68620e330670f88b14
Kassahun Takele <kass3me@gmail.com>
2024-02-16 19:26:10 +0300

    port  changed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ methods: ['PUT', 'GET', 'PATCH', 'POST', 'OPTIONS'] });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
